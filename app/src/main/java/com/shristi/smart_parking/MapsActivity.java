be71bb722e8042bbd428f555b625d313c5f38061
ShristiAgrawal <agrawalshristi2@gmail.com>
2021-03-16 20:04:53 +0530

    Initial commit

---
+++ app/src/main/java/com/shristi/smart_parking/MapsActivity.java
@@ -0,0 +1,63 @@
+package com.shristi.smart_parking;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng mylocation = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(mylocation).title("I am here"));
+        LatLng member1 = new LatLng(-33, 151);
+        LatLng member2 = new LatLng(-33.5, 151);
+        LatLng member3 = new LatLng(-33.25, 151);
+        LatLng member4 = new LatLng(-33, 151.5);
+        mMap.addMarker(new MarkerOptions().position(member1)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).title("Member1 here"));
+        mMap.addMarker(new MarkerOptions().position(member2)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_BLUE)).title("Member2 here"));
+        mMap.addMarker(new MarkerOptions().position(member3)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)).title("Member3 here"));
+        mMap.addMarker(new MarkerOptions().position(member4)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).title("Member4 here"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mylocation));
+    }
+}
