19969ac32d6fb7419060f7ef190a1c2d3ada7e29
ShristiAgrawal <agrawalshristi2@gmail.com>
2021-03-16 19:29:20 +0530

    Initial commit

---
+++ app/src/main/java/com/shristi/smart_parking/MainActivity.java
@@ -0,0 +1,40 @@
+package com.shristi.smart_parking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText id,password;
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        id=findViewById(R.id.id);
+        password=findViewById(R.id.password);
+        login=findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+    private void login() {
+        Intent intent=new Intent(MainActivity.this,options.class);
+                   startActivity(intent);
+
+
+            }
+
+
+
+
+
+}
