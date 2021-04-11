28c562ec8717c915b3d7e4cc3fd0a45e6e648d8c
nil0481 <nileshsinghania0481@gmail.com>
2021-04-11 23:58:50 +0530

    testact

--- app/src/main/java/com/shristi/smart_parking/ParkingSlots.java
+++ app/src/main/java/com/shristi/smart_parking/ParkingSlots.java
@@ -2,14 +2,27 @@ package com.shristi.smart_parking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ParkingSlots extends AppCompatActivity {
-
+    ImageButton imageButton1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_slots);
+        imageButton1=findViewById(R.id.imageButton1);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(ParkingSlots.this,BookingAct.class);
+                startActivity(i);
+
+            }
+        });
     }
+
 
 }
